f5578733c2fd1db9c631236cbba4d5ab91a4164c
Gredja <gredja.a@gmail.com>
2021-06-15 20:12:29 +0300

    Second commit

--- Locators/Locators/UnitTest1.cs
+++ Locators/Locators/UnitTest1.cs
@@ -1,13 +1,22 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 
 namespace Locators
 {
     public class Tests
     {
+        private IWebDriver _driver;
+        private IJavaScriptExecutor _js;
+        private Actions _mouseAction;
+
         [SetUp]
         public void Setup()
         {
+            _driver = new ChromeDriver("C:\\");
         }
+
 
         [Test]
         public void Test1()
